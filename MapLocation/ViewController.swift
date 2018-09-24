f54ca04e959af50c4047349a865a62d448485032
Kub <kub@Kub-MacBook-Pro.local>
2018-09-24 13:35:22 +0300

    Map için koordinat ve span verildi

--- MapLocation/ViewController.swift
+++ MapLocation/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import  MapKit
+import  CoreLocation
+
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //koordinat bilgisi
+        var location = CLLocationCoordinate2DMake(41.043999, 29.002001)
+        var span=MKCoordinateSpanMake(0.003,0.003)
+        var region=MKCoordinateRegion(center: location, span: span)
+        map.setRegion(region, animated: true)
+        
+        let aciklama=MKPointAnnotation()
+        aciklama.coordinate=location
+        aciklama.title="İstanbul"
+        aciklama.subtitle="Beşiktaş"
+        map.addAnnotation(aciklama)
+        //haritada yer etiketleme
+
+    
     }
 
     override func didReceiveMemoryWarning() {
